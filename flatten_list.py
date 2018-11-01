22440268a9fb1dbc33adc238b382212a317ea0f4
leepand <85721094@qq.com>
2018-11-01 09:10:56 +0800

    add funcs

---
+++ flatten_list.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*- 
+#拉平多个list
+
+def flatten(x):
+    """
+    >>> flatten([[1]])
+    [1]
+    >>> flatten([[1, 2], [3, 4]])
+    [1, 2, 3, 4]
+    >>> flatten([[[1], [2]], [[3], [4]]])
+    [1, 2, 3, 4]
+    """
+    if isinstance(x[0], list):
+        return list(toolz.concat(map(flatten, x)))
+    else:
+        return x
