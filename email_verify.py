447a7d08451f347f2bc61ab117f1d1b71c57d1b1
leepand <85721094@qq.com>
2019-03-11 15:12:46 +0800

    email verify

---
+++ email_verify.py
@@ -0,0 +1,6 @@
+import re
+emailRegex = re.compile(r"(?:^|\s)[-a-z0-9_.]+@(?:[-a-z0-9]+\.)+[a-z]{2,6}(?:\s|$)", re.IGNORECASE)
+
+def verifyEmailAddy(x) :
+    return (emailRegex.match(x) is not None)
+verifyEmailAddy('padd@163.com')
