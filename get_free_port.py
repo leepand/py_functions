d644fa7e56d06817490f986e4214e4c3dbf8ef10
leepand <85721094@qq.com>
2019-06-22 14:20:36 +0800

    get free port use socket

---
+++ get_free_port.py
@@ -0,0 +1,13 @@
+import socket
+
+def get_free_port():
+    try:
+        s = socket.socket()
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('localhost', 0))
+        (hostname, port) = s.getsockname()
+        s.close()
+        return (hostname, port)
+    except socket.error:
+        return (None, None)
+get_free_port()
