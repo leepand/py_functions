04aebcb30dabab0d9a5584b8e8649cfb1520f755
leepand <85721094@qq.com>
2019-10-16 18:52:56 +0800

    add catch err

--- catch_err.py
+++ catch_err.py
@@ -1,3 +1,24 @@
+level = 0
+def trace(f):
+    def g(*args):
+        global level
+
+        # pretty print indicating the level
+        prefix = "|  " * level + "|--"
+        strargs = ", ".join(repr(a) for a in args)
+        print("{} {}({})".format(prefix, f.__name__, strargs))
+
+        # increment the level before calling the function
+        # and decrement it after the call
+        level += 1
+        try:
+            result = f(*args)
+        except Exception as e:
+            result = e
+        level -= 1
+
+        return result
+    return g
 from functools import wraps
 
 def catch_algolink_exception(func):
