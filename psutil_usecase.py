98d24777c7b1005f7b558eaa6ff35e545e556271
leepand <85721094@qq.com>
2019-12-02 10:24:35 +0800

    add psutil_usecase.py

---
+++ psutil_usecase.py
@@ -0,0 +1,19 @@
+def redis_daemon_exist():
+    # We can just check of 'redis-server' process because the default
+    # situation is that we are in a container without any other python2
+    # process.
+    import psutil
+    pids = psutil.pids()
+    process_names = []
+
+    for pid in pids:
+        try:
+            name = psutil.Process(pid).name()
+        except psutil.NoSuchProcess:
+            name = None
+        process_names.append(name)
+
+    return 'redis-server' in process_names
+
+redis_daemon_exist()
+
