fd6be432a2bf4f6567b7cd650e7835b67a7ddc4f
leepand <85721094@qq.com>
2020-12-03 16:52:09 +0800

    add lazyloader.py

---
+++ lazyloader.py
@@ -0,0 +1,26 @@
+import importlib
+import types
+class LazyLoader(types.ModuleType):
+    def __init__(self, module_name='os', submod_name=None):
+        self._module_name = '{}{}'.format(
+            module_name,
+            submod_name and '.{}'.format(submod_name) or ''
+        )
+        self._mod = None
+        super(LazyLoader, self).__init__(self._module_name)
+        
+    def _load(self):
+        if self._mod is None:
+            self._mod = importlib.import_module(
+                self._module_name
+            )
+        return self._mod
+
+    def __getattr__(self, attrb):
+        return getattr(self._load(), attrb)
+
+    def __dir__(self):
+        return dir(self._load())
+
+_os  = LazyLoader("os")
+_os.listdir("./")
