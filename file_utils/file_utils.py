068a71e39e881f7ab17b1606f8e5f02db5d0368c
leepand <leepand@leepanddeMacBook-Pro.local>
2022-05-11 11:53:52 +0800

    add fileutils

---
+++ file_utils/file_utils.py
@@ -0,0 +1,19 @@
+import os
+import errno
+
+def mkdir(root, name=None):
+    """
+    Make directory with name "root/name", or just "root" if name is None.
+    :param root: Name of parent directory
+    :param name: Optional name of leaf directory
+    :return: Path to created directory
+    """
+    target = os.path.join(root, name) if name is not None else root
+    try:
+        os.makedirs(target)
+    except OSError as e:
+        if e.errno != errno.EEXIST or not os.path.isdir(target):
+            raise e
+    return target
+
+mkdir("tmp","aah2")
