f8b007e369e4c4bfb214310859468aa09edffc68
leepand <leepand@leepanddeMacBook-Pro.local>
2022-06-20 09:19:29 +0800

    add dot_flatten

---
+++ dict_utils/.ipynb_checkpoints/dot_flatten-checkpoint.py
@@ -0,0 +1,19 @@
+import importlib
+from logging import getLogger
+
+log = getLogger(__name__)
+
+def dot_flatten(d):
+    try:
+        from flatten_dict import flatten
+
+        d = flatten(d, reducer="dot", enumerate_types=(list,))
+    except Exception:
+        log.warning(
+            "{} failed to be flattened. To install dependency, you can run: pip install flatten-dict>=0.3.0".format(
+                d
+            ),
+            exc_info=True,
+        )
+    return d
+dot_flatten({"x":{"y":0}})
