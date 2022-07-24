4bab64ff5606eb9a26f9cbe6e41dfe860f4f09e1
leepand <85721094@qq.com>
2022-07-24 13:22:38 +0800

    from datetime import datetime
    from sqlalchemy import Column, DateTime
    from flask_sqlalchemy import SQLAlchemy, Model
    
    class BaseModel(Model):
        # create_at这个属性也是创建表结构默认都包含的
        create_at = Column(DateTime, default=datetime.utcnow())
    
        def to_dict(self):
            columns = self.__table__.columns.keys()
            return {key: getattr(self, key) for key in columns}
    
    db = SQLAlchemy(model_class=BaseModel)

---
+++ sql_to_dict.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime
+from flask_sqlalchemy import SQLAlchemy, Model
+
+
+class BaseModel(Model):
+    # create_at这个属性也是创建表结构默认都包含的
+    create_at = Column(DateTime, default=datetime.utcnow())
+
+    def to_dict(self):
+        columns = self.__table__.columns.keys()
+        return {key: getattr(self, key) for key in columns}
+
+
+db = SQLAlchemy(model_class=BaseModel)
