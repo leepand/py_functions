419f22b161773e40f790ed7ababf0a35719c1fd6
leepand <leepand@leepanddeMacBook-Pro.local>
2023-02-16 15:03:10 +0800

    add list_util.py

---
+++ rl_utils/list_util.py
@@ -0,0 +1,11 @@
+def duplicate_removal(src_list):
+    name_scope = set()
+    dest_list = []
+    for item in src_list:
+        if item in name_scope:
+            continue
+        name_scope.add(item)
+        dest_list.append(item)
+    return dest_list
+
+
